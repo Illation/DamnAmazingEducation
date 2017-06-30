2004952a9239f5b4b641bcbca72b20c9582cb75e
kiamvdd <kiam.vandendriessche@student.howest.be>
2017-07-01 00:01:19 +0200

    no message

---
+++ JustATest/Assets/Scripts/GenericItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericItem : MonoBehaviour, IItem
+{
+    public bool Grab(Transform origin)
+    {
+        transform.SetParent(origin);
+        return true;
+    }
+
+    public bool Release()
+    {
+        transform.SetParent(null);
+        return true;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
