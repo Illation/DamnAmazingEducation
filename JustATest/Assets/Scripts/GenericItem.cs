2e66ebd9802340a07dbd1fa469280c65290e0762
Robert Lindner <contact@robert-lindner.com>
2017-07-01 10:19:07 +0100

    Premerge

--- JustATest/Assets/Scripts/GenericItem.cs
+++ JustATest/Assets/Scripts/GenericItem.cs
@@ -7,12 +7,14 @@ public class GenericItem : MonoBehaviour, IItem
     public bool Grab(Transform origin)
     {
         transform.SetParent(origin);
+        transform.localPosition = Vector3.zero;
         return true;
     }
 
     public bool Release()
     {
-        transform.SetParent(null);
+        ObjectController objCont = this.GetComponent<ObjectController>();
+        if (objCont != null) objCont.Destroy();
         return true;
     }
     // Use this for initialization
