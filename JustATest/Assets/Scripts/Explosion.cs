922939842cb359d44605e89c21c6fbe244d268bd
Robert Lindner <contact@robert-lindner.com>
2017-07-01 17:39:14 +0100

    Explosions, End game state

---
+++ JustATest/Assets/Scripts/Explosion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    [SerializeField]
+    float Lifetime = 0.5f;
+    float _timer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _timer = Lifetime;		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _timer -= Time.deltaTime;        		
+        if(_timer < 0)
+        {
+            foreach(GameObject child in this.transform)
+            {
+                Destroy(child);
+            }
+            Destroy(this);
+        }
+	}
+}
