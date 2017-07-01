31600185a425800e1a237c72e1dd132b3844ecdf
kiamvdd <kiam.vandendriessche@student.howest.be>
2017-07-01 19:44:02 +0200

    Updated EMP

--- JustATest/Assets/Scripts/EMPItem.cs
+++ JustATest/Assets/Scripts/EMPItem.cs
@@ -12,13 +12,19 @@ public class EMPItem : MonoBehaviour, IItem
     private bool _attached;
     private Thruster _targetThruster;
     private int _ownerNum;
-    private float _timer;
+    private float _explosionTimer = 0;
+    private float _selectionTimer = 0;
+    public float SelectionSwitchDelay = 0.3f;
     public float ExplosionTime = 2.0f;
+    private bool _selectingTarget = true;
+    private int _thrusterID = 0;
+    private int _maxThrusterID = 0;
     public bool Grab(Transform origin)
     {
         if (!_thrown)
         {
             _wall = GameObject.Find("Wall").GetComponent<WallController>();
+            _maxThrusterID = _wall.LeftThrusters.Count - 1;
             _throwingPlayer = origin.root.gameObject;
 
             transform.SetParent(origin);
@@ -39,52 +45,55 @@ public class EMPItem : MonoBehaviour, IItem
 
     public bool Release()
     {
-        List<Thruster> thrusters;
-        if (_ownerNum == 1)
+        if (!_thrown)
         {
-            thrusters = _wall.LeftThrusters;
-        }
-        else
-        {
-            thrusters = _wall.RightThrusters;
-        }
-
-        foreach (Thruster t in thrusters)
-        {
-            if (t.IsLoaded)
+            List<Thruster> thrusters;
+            if (_ownerNum == 1)
             {
-                _targetThruster = t;
-                continue;
+                thrusters = _wall.LeftThrusters;
             }
+            else
+            {
+                thrusters = _wall.RightThrusters;
+            }
+
+            _targetThruster = thrusters[_thrusterID];
+            _startingPoint = transform.position;
+            transform.SetParent(null);
+            _thrown = true;
         }
 
-        if (_targetThruster == null)
-        {
-            return false;
-        }
 
-
-        _startingPoint = transform.position;
-        transform.SetParent(null);
-
-
-        _thrown = true;
         return true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_selectingTarget)
+        {
+            _selectionTimer += Time.deltaTime;
+            if (_selectionTimer >= SelectionSwitchDelay)
+            {
+                _selectionTimer = 0;
+                ++_thrusterID;
+                if (_thrusterID > _maxThrusterID)
+                {
+                    _thrusterID = 0;
+                }
+            }
+        }
+
         if (_attached)
         {
-            _timer += Time.deltaTime;
+            _explosionTimer += Time.deltaTime;
 
-            if (_timer > ExplosionTime)
+            if (_explosionTimer > ExplosionTime)
             {
                 Explode();
             }
         }
-        if (_thrown)
+        else if (_thrown)
         {
             Vector3 posSelf = transform.position;
             posSelf.y = 0;
@@ -118,7 +127,7 @@ public class EMPItem : MonoBehaviour, IItem
 
     void Explode()
     {
-        
+        _targetThruster.Discharge();
         ObjectController objCont = this.GetComponent<ObjectController>();
         if (objCont != null) objCont.Destroy();
     }
