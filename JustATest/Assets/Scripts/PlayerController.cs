8e26623cd3c081945c2280cd061a7fd320861c9c
HUNTER-PC\Arno <arnovandenbranden@gmail.com>
2017-07-01 10:19:33 +0200

    Added Player two and its controls

--- JustATest/Assets/Scripts/PlayerController.cs
+++ JustATest/Assets/Scripts/PlayerController.cs
@@ -4,41 +4,37 @@
 /// <summary>
 /// Controls all the actions Toad can make.
 /// </summary>
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     // Fields
     // ------
 
     // Public variables
-    public float movementSpeed = 10.0f; // How fast the player moves..
-    public Camera cam = null;           // Reference to the Main Camera.
-    public float interactDistance = 0.5f; // How far Toad can interact with objects
+    public float movementSpeed = 10.0f; // How fast the player moves
+    public Camera cam = null;           // Reference to the Main Camera
+    public float interactDistance = 0.5f; // How far player can interact with objects
+    public bool playerTwo = false;
 
     // Private variables
-    private CharacterController _controller = null; // Reference to the character controller.
-    private Vector3 _moveDirection = Vector3.zero;  // Which direction he's moving.
-    private float _sqrInteractDistance = 0;         // Interact distance squared, for performance.               
+    private CharacterController _controller = null; // Reference to the character controller
+    private Vector3 _moveDirection = Vector3.zero;  // Which direction he's moving
+    private float _sqrInteractDistance = 0;         // Interact distance squared, for performance.              
 
     // Methods
     // -------
 
-    void Start()
-    {
+    void Start() {
         _controller = GetComponent<CharacterController>();
         _sqrInteractDistance = interactDistance * interactDistance;
     }
 
-    void Update()
-    {
+    void Update() {
         HandleMovement(); // Handle all the movement.
         HandleInput(); // Handle all the input.
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
+    void OnControllerColliderHit(ControllerColliderHit hit) {
         // Check what the controller hit and act accordingly.
-        switch (hit.gameObject.tag)
-        {
+        switch (hit.gameObject.tag) {
             //case "Untagged":
             //    return;
             //default:
@@ -48,9 +44,9 @@ public class PlayerController : MonoBehaviour
     }
 
     // Handles all the movement.
-    void HandleMovement()
-    {
-        _moveDirection = Input.GetAxisRaw("Vertical") * cam.transform.forward + Input.GetAxisRaw("Horizontal") * cam.transform.right;
+    void HandleMovement() {
+        if (!playerTwo) _moveDirection = Input.GetAxisRaw("Vertical") * cam.transform.forward + Input.GetAxisRaw("Horizontal") * cam.transform.right;
+        else _moveDirection = Input.GetAxisRaw("Vertical 2") * cam.transform.forward + Input.GetAxisRaw("Horizontal 2") * cam.transform.right;
         _moveDirection.y = 0;
         if (_moveDirection != Vector3.zero) transform.rotation = Quaternion.LookRotation(_moveDirection);
         _moveDirection.Normalize();
@@ -59,10 +55,9 @@ public class PlayerController : MonoBehaviour
     }
 
     // Handle all the necessary input.
-    void HandleInput()
-    {
-            // Use A button on controller or E key to interact with closest object
-            // if (Input.GetButtonDown("Interact")) Interact();
+    void HandleInput() {
+        // Use A button on controller or E key to interact with closest object
+        // if (Input.GetButtonDown("Interact")) Interact();
     }
 
     //// Interact with the closest, reachable interactable object.
