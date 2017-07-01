2dcedc7d0035375f6ec8bc53331e09c7676cf0b5
kiamvdd <kiam.vandendriessche@student.howest.be>
2017-07-01 14:53:58 +0200

    merge

--- JustATest/Assets/Scripts/Thruster.cs
+++ JustATest/Assets/Scripts/Thruster.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thruster : MonoBehaviour {
+public class Thruster : MonoBehaviour
+{
+
+    public float Thrust = 0;
+    public bool IsLoaded = false;
+
+    [SerializeField]
+    private float ThrustDeceleration = 0.25f;
+    private float _thrustDec = 0;
+
+    [SerializeField]
+    ParticleSystem ThrustParticles;
+    [SerializeField]
+    float ThrustParticleLifetime = 2;
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(Thrust > 0)
+        {
+            _thrustDec += ThrustDeceleration*Time.deltaTime;
+            Thrust -= _thrustDec * Time.deltaTime;
+            if(Thrust<0)
+            {
+                _thrustDec = 0;
+                Thrust = 0;
+            }
+        }
+        ThrustParticles.startLifetime = ThrustParticleLifetime * Thrust;
 	}
+
+    public void Activate()
+    {
+        Thrust = 1;
+    }
 }
