835f4c6c5076778619f6b373632f6cd43eec8dd3
Robert Lindner <contact@robert-lindner.com>
2017-07-02 15:10:59 +0100

    Upgrade timer functionality

---
+++ JustATest/Assets/TimerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+
+    [SerializeField]
+    Text TimerText;
+    WallController _wall;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _wall = GameObject.Find("Wall").GetComponent<WallController>();		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TimerText.text = ((int)_wall.UpgradeTimer).ToString();	
+	}
+}
