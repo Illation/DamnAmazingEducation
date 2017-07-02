32a8dcb4728e63377c668e5db8706d629322f54b
Robert Lindner <contact@robert-lindner.com>
2017-07-02 15:52:55 +0100

    Respawn magnet

---
+++ JustATest/Assets/MagnetController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetController : MonoBehaviour {
+
+    [SerializeField]
+    PlayerController Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.parent.position + new Vector3(0, 1 + Player.ArmHeight, 0);	
+	}
+}
